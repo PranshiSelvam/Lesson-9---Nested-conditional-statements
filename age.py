1da43d3602e37caa60516e827480d12b44863f19
PranshiSelvam <pranshiselvam11@gmail.com>
2024-10-28 14:51:41 +0530

    nested conditions

---
+++ age.py
@@ -0,0 +1,8 @@
+
+
+age = int(input("Please enter your age: "))
+
+if 10 <= age <= 20:
+        print("You can sign up for the magazine collection website!")
+else:
+        print("Sorry, you are not eligible to sign up for the magazine collection website.")
